36a4e338d8f9af0dec8b6e66b1df40da2411a40f
develop <patinetakiller564@gmail.com>
2025-08-08 14:47:16 -0300

    Se completo los controladores para crear User

---
+++ src/controllers/taskControllers.js
@@ -0,0 +1,11 @@
+import { Task } from "../modules/taskModules.js";
+
+export const taskCreate = (req,res) =>{
+
+    try {
+        
+    } catch (error) {
+        
+    }
+
+}
--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,44 +1,79 @@
-import { sequelize } from "../config/tasks_users_db.js";
 import { User } from "../modules/userModules.js";
-import { Task } from "../modules/taskModules.js";
+
 
 export const createUser = async (req,res) =>{
+
 
     try{
         const {name,email,password}= req.body
 
-        //name
+    //name
         if (name === null && name === "" && name === undefined)return res.status(400).json(
             {Message:"El NOMBRE no puede contener parametros Nulos,Vacíos o Indefinidos"}
         );
 
        const nameLargo = (name)=>{
-        if (User.name.length>100)
+        if (name.length>100)
             name = nameLargo
          console.log("El nombre es muy largo")
-         return res.status(400).json({message:"EL nombre no puede contener mas de 100 caracteres"});
-       }
-
-       //
+         return res.status(400).json({message:"EL nombre no puede contener mas de 100 caracteres"})};
+    //
 
 
-
-       //
-
-
-       //email
-       if (email === null && email === "" && email === undefined)return res.status(400).json(
+    //email
+       if (email === null && email === "" && email === undefined)
+        return res.status(400).json(
             {Message:"El EMAIL no puede contener parametros Nulos,Vacíos o Indefinidos"}
         ); 
+        
+        const emailExiste = await User.findOne({where: {email: req.params.email}})
 
-         const emailExiste = await email => {
+        if (emailExiste) 
+         return res.status(400).json(
+            {Message:"Este correo ya está en uso"});
 
-        }
+
+            
+        const emailLargo = (email)=>{
+        if (email.length>100)
+            email = emailLargo
+         console.log("El email es muy largo")
+         return res.status(400).json({message:"EL Email no puede contener mas de 100 caracteres"})};
+    //
 
 
 
-    }
-    }
+    //password
+    
+       if (password === null && password === "" && password === undefined)
+        return res.status(400).json(
+            {Message:"El PASSWORD no puede contener parametros Nulos,Vacíos o Indefinidos"}
+        ); 
+
+        const passwordlLargo = (password)=>{
+        if (password.length>100)
+            password = passwordlLargo
+         console.log("El password es muy largo")
+         return res.status(400).json({message:"EL Password no puede contener mas de 100 caracteres"})};
+    //
+
+    const user = User.create({name,email,password})
+    if (user) return res.status(200).json({Message:"Se ha creado el Usuario",user});
+
+
+    
+
+
+
+
+
+
+
+
+
+
+}catch (error) {Message:"Error por parte del servidor al crear el User"}
+};
 
 
 
